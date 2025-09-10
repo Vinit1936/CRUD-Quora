b96f97fd47f6ade6bf42167b5bb36f1886bb48f0
Vinit Patil <vinitdpatilwork193@gmail.com>
2025-09-11 00:47:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const methodOverride = require('method-override')
 
 
-app.use(express.urlencoded({extended: true}));//To let express understand url encoded parth $this is a Middle ware
+app.use(express.urlencoded({extended: true})); 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
